0ad265d491de4c642b2c7960d94dda6647bb87e3
mapc1508 <mapc1508@aubih.edu.ba>
2014-06-01 17:12:57 +0200

    Creating Person and Student classes

---
+++ Student180/Student180/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student180
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student student1 = new Student();
+            student1.Name = "Mirza";
+            
+
+            Student student2 = new Student("Mirza", "Pecanac", "miki@hotmail.com", "SA");
+            Student student3 = new Student("Amira", "Halilovic", "amira@bih.net.ba");
+            
+            Console.WriteLine(student2.GetStudentInfo());
+            Console.WriteLine(student3.GetStudentInfo());
+            Console.WriteLine(student1.GetStudentInfo());
+           
+
+
+
+            
+
+
+            
+        
+        
+        }
+    }
+}
---
+++ Student180/Student180/Student.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student180
+{
+    class Person
+    {
+        protected string name, lastName;
+        public Person()
+        {
+            
+        }
+        public Person(string name, string lastName)
+        {
+            this.name = name;
+            this.lastName = lastName;
+        }
+        public string getPersonInfo()
+        {
+            return name + " , " + lastName;
+        }
+    
+    }
+    
+    class Student : Person 
+    {
+
+
+        public string Name
+        {
+            get
+            {
+                return base.name;
+            }
+            set
+            {
+                if (SetName(value))
+                {
+                    base.name = value;
+                }
+                else
+                    base.name = "";
+            }
+        }
+        public string Email { get; set; }
+        private string location;
+        public string Location
+        {
+            get
+            {
+                if (location == "SA") location = "Sarajevo";
+                else if (location == "TZ") location = "Tuzla";
+                else location = "Other";
+
+                return location;
+            }
+
+            set
+            {
+
+                if (value == "SA" || value == "SARAJEVO" || value == "Sarajevo")
+                    location = "SA";
+                else if (value == "TZ" || value == "Tuzla")
+                    location = "TZ";
+                else
+                    location = "NA";
+            }
+
+        }
+       
+        
+        public Student()
+        {
+        }
+        public Student(string name, string lastName, string email) : base(name, lastName) 
+        {
+
+            Name = name;
+            Email = email;
+        }
+        public Student(string name, string lastName, string email, string location): base(name, lastName)       
+        {
+            Name = name;
+            this.location = location;
+            Email = email;
+        }
+
+        ~Student()
+        {
+            ;
+        }
+
+        public bool SetName(string input)
+        {
+            char[] charArray = input.ToCharArray();
+                      
+            if (input.Length < 2)
+            {
+                Console.WriteLine("Name must be at least two characters long");
+
+                return false; 
+            }
+                     
+            foreach( char c in charArray)
+            {
+                if (!char.IsLetter(c))
+                {
+                    Console.WriteLine("Name can only have letters");
+
+                    return false;
+                }               
+            }
+
+            if (!char.IsUpper(charArray[0]))
+            {
+                Console.WriteLine("Name must start with an uppercase letter");
+                return false;
+            }
+            return true;              
+        }
+
+        public string GetStudentInfo()
+        {
+            return getPersonInfo() + " , " + Email + " , " + Location;
+        }      
+    }
+}
