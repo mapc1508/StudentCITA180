e99330324d022d37fea17e3fcd6e0d60db1cbf19
mapc1508 <mapc1508@aubih.edu.ba>
2014-06-02 12:15:42 +0200

    Assembly Info Update

--- Student180/Student180/Properties/AssemblyInfo.cs
+++ Student180/Student180/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Student180")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Mirza Pecanac")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mapc1508@aubih.edu.ba")]
 [assembly: AssemblyProduct("Student180")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
